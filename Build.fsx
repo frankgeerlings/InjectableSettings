4eaad2cef1065dc0db854abfb7991f7b196a41ab
Frank Geerlings <frank@frankgeerlings.com>
2013-06-14 20:13:08 +0200

    F# make (FAKE) build script.

---
+++ Build.fsx
@@ -0,0 +1,24 @@
+﻿// include Fake lib
+#r @"packages\FAKE.1.74.196.0\tools\FakeLib.dll"
+open Fake 
+
+// Properties
+let buildDir = @".\Build\"
+
+// Targets
+Target "Clean" (fun _ ->
+    CleanDir buildDir
+)
+ 
+Target "BuildApp" (fun _ ->
+    !! @"InjectableSettings\**\*.csproj"
+      |> MSBuildRelease buildDir "Build"
+      |> Log "AppBuild-Output: "
+)
+
+// Dependencies
+"Clean"
+  ==> "BuildApp"
+
+// start build
+Run "BuildApp"
