f2add78795ec6fd790863c7fcd19472472f00445
Frank Geerlings <frank@frankgeerlings.com>
2013-06-14 15:56:41 +0200

    Initial commit.

---
+++ InjectableSettings.Test/TestConfigurationSetting.cs
@@ -0,0 +1,58 @@
+﻿namespace InjectableSettings.Test
+{
+	using System.Configuration;
+
+	using FluentAssertions;
+
+	using NUnit.Framework;
+
+	[TestFixture]
+	public class TestConfigurationSetting
+	{
+		public class TestableConfigurationSetting : ConfigurationSetting { }
+
+		public class SettingWithDefaultConfigurationSetting : ConfigurationSetting
+		{
+			public SettingWithDefaultConfigurationSetting() : base("Default value") {}
+		}
+
+		[Test]
+		public void ConfigurationSettingsAreReadFromAppConfig()
+		{
+			// Arrange
+			ConfigurationManager.AppSettings.Set("Testable", "Value");
+
+			// Act
+			var sut = new TestableConfigurationSetting();
+
+			// Assert
+			sut.Value.Should().Be("Value");
+		}
+
+		[Test]
+		public void ConfigurationSettingsSupportDefaultValues()
+		{
+			// Arrange
+			ConfigurationManager.RefreshSection("appSettings");
+
+			// Act
+			var sut = new SettingWithDefaultConfigurationSetting();
+
+			// Assert
+			sut.Value.Should().Be("Default value");
+		}
+
+		[Test]
+		public void ConfigurationSettingsWithoutDefaultValuesHaveNullValues()
+		{
+			// Arrange
+			ConfigurationManager.RefreshSection("appSettings");
+
+			// Act
+			var sut = new TestableConfigurationSetting();
+
+			// Assert
+			sut.Value.Should().BeNull();
+		}
+	}
+}
---
+++ InjectableSettings/ConfigurationSetting.cs
@@ -0,0 +1,28 @@
+﻿namespace InjectableSettings
+{
+	using System;
+	using System.Configuration;
+
+	public class ConfigurationSetting
+	{
+		public string Value { get; set; }
+
+		protected ConfigurationSetting() : this(null) { }
+
+		protected ConfigurationSetting(string defaultValue)
+		{
+			var key = GetAppSettingsKeyNameFromTypeName();
+			this.Value = ConfigurationManager.AppSettings[key] ?? defaultValue;
+		}
+
+		private string GetAppSettingsKeyNameFromTypeName()
+		{
+			var key = this.GetType().Name.Replace("ConfigurationSetting", "");
+			if (key == this.GetType().Name)
+			{
+				throw new Exception("Configuration setting type name must end with 'ConfigurationSetting'.");
+			}
+			return key;
+		}
+	}
+}
