51befb6f876cecc3d6e5c63a74cf8963bbc817ce
Frank Geerlings <frank@frankgeerlings.com>
2013-06-20 10:04:38 +0200

    Build NuGet package with FAKE. Improved package specification.

--- Build.fsx
+++ Build.fsx
@@ -4,10 +4,12 @@ open Fake
 
 // Properties
 let buildDir = @".\Build\"
+let nugetDir = @".\NuGet\"
 
 // Targets
 Target "Clean" (fun _ ->
     CleanDir buildDir
+    CleanDir nugetDir
 )
  
 Target "BuildApp" (fun _ ->
@@ -16,9 +18,22 @@ Target "BuildApp" (fun _ ->
       |> Log "AppBuild-Output: "
 )
 
+Target "BuildNuGet" (fun _ -> 
+    let nugetLibDir = nugetDir @@ "lib/net20"
+        
+    XCopy buildDir nugetLibDir
+
+    NuGet (fun p -> 
+            { p with
+                OutputPath = nugetDir
+                Version = "1.0.0"
+                Publish = false })  "InjectableSettings.nuspec"
+)
+
 // Dependencies
 "Clean"
   ==> "BuildApp"
+  ==> "BuildNuGet"
 
 // start build
-Run "BuildApp"
+Run "BuildNuGet"
